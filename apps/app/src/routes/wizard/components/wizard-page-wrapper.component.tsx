857b7f37138b32c23310e662890b3e60b886ce2c
alex-struk <109102252+alex-struk@users.noreply.github.com>
2023-10-25 16:02:50 -0700

    Update wizard-page-wrapper.component.tsx

--- apps/app/src/routes/wizard/components/wizard-page-wrapper.component.tsx
+++ apps/app/src/routes/wizard/components/wizard-page-wrapper.component.tsx
@@ -1,7 +1,14 @@
 import { Col, Row, Space } from 'antd';
+import { ReactNode } from 'react';
 import { PageHeader } from '../../../components/app/page-header.component';
 
-export const WizardPageWrapper = ({ title, subTitle, children }) => (
+interface WizardPageWrapperProps {
+  title: string;
+  subTitle: string;
+  children: ReactNode;
+}
+
+export const WizardPageWrapper: React.FC<WizardPageWrapperProps> = ({ title, subTitle, children }) => (
   <Space direction="vertical" style={{ width: '100%' }} size="large">
     <PageHeader title={title} subTitle={subTitle} />
     <div style={{ margin: '0 1rem 1rem' }}>
