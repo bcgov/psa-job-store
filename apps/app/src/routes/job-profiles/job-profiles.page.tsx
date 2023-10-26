a3808aa8f026fc3669905e1c1c3730b42d54fd69
alex-struk <109102252+alex-struk@users.noreply.github.com>
2023-10-26 11:14:13 -0700

    refactored search into a separate component

--- apps/app/src/routes/job-profiles/job-profiles.page.tsx
+++ apps/app/src/routes/job-profiles/job-profiles.page.tsx
@@ -1,111 +1,17 @@
-import { FileTextFilled } from '@ant-design/icons';
-import { Breakpoint, Col, Empty, Grid, Row, Space, Typography } from 'antd';
-import { useEffect } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
-import { PageHeader } from '../../components/app/page-header.component';
-import { useLazyGetJobProfilesQuery } from '../../redux/services/graphql-api/job-profile.api';
-import { JobProfileSearchResults } from './components/job-profile-search-results.component';
-import { JobProfileSearch } from './components/job-profile-search.component';
-import { JobProfile } from './components/job-profile.component';
-
-const { Text, Title } = Typography;
-const { useBreakpoint } = Grid;
+import { PageHeader } from '@ant-design/pro-layout';
+import { Space } from 'antd';
+import { useSearchParams } from 'react-router-dom';
+import JobProfiles from './components/job-profiles.component';
 
 export const JobProfilesPage = () => {
   const [searchParams] = useSearchParams();
-  const [trigger, { data, isLoading }] = useLazyGetJobProfilesQuery();
-
-  useEffect(() => {
-    const search = searchParams.get('search')?.replace(/(\w)\s+(\w)/g, '$1 <-> $2');
-
-    trigger({
-      where: {
-        AND: [
-          {
-            ...(search != null && {
-              title: {
-                search,
-              },
-              context: {
-                search,
-              },
-              overview: {
-                search,
-              },
-            }),
-          },
-        ],
-      },
-    });
-  }, [searchParams, trigger]);
-
-  const params = useParams();
-  const screens: Partial<Record<Breakpoint, boolean>> = useBreakpoint();
-
-  const renderJobProfile = () => {
-    return params.id ? (
-      <JobProfile />
-    ) : (
-      <div style={{ marginTop: '16rem' }}>
-        <Empty
-          description={
-            <Space direction="vertical" style={{ userSelect: 'none' }}>
-              <Title level={1}>Select a Job Profile</Title>
-              <Text>Nothing is selected</Text>
-            </Space>
-          }
-          image={<FileTextFilled style={{ fontSize: '60pt' }} />}
-        />
-      </div>
-    );
-  };
+  const searchQuery = searchParams.get('search');
 
   return (
     <>
       <Space direction="vertical" style={{ width: '100%' }} size="large">
         <PageHeader title="Job Profiles" subTitle="Find a Job Profile which suits your needs" />
-        <JobProfileSearch />
-        <div style={{ margin: '0 1rem' }}>
-          <Row justify="center" gutter={16}>
-            {screens['xl'] === true ? (
-              <>
-                <Col span={8}>
-                  <JobProfileSearchResults data={data} isLoading={isLoading} />
-                </Col>
-                <Col span={16}>{renderJobProfile()}</Col>
-              </>
-            ) : params.id ? (
-              <Col span={24}>{renderJobProfile()}</Col>
-            ) : (
-              <JobProfileSearchResults data={data} isLoading={isLoading} />
-            )}
-          </Row>
-
-          {/* <Row justify="center" gutter={16}>
-            <Col lg={24}>{params.id ? <JobProfile /> : <JobProfileSearchResults data={data} />}</Col>
-          </Row> */}
-
-          {/* <Row justify="center" gutter={16}>
-            <Col lg={params.id ? 0 : 24} xl={8}>
-              <JobProfileSearchResults data={data} />
-            </Col>
-            <Col lg={params.id ? 0 : 24} xl={14}>
-              {params.id ? (
-                <JobProfile />
-              ) : (
-                <Empty
-                  description={
-                    <Space direction="vertical" style={{ marginTop: '16rem', userSelect: 'none' }}>
-                      <Title level={1}>Select a Job Profile</Title>
-                      <Text>Nothing is selected</Text>
-                    </Space>
-                  }
-                  image={<FileTextFilled style={{ fontSize: '60pt' }} />}
-                />
-              )}
-            </Col>
-          </Row> */}
-        </div>
+        <JobProfiles searchQuery={searchQuery} />
       </Space>
     </>
   );
