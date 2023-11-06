811a972bb5cc9e26610442e8bb3c60a2249ccc98
alex-struk <109102252+alex-struk@users.noreply.github.com>
2023-11-06 15:18:16 -0800

    Redirect to original page after login

--- apps/app/src/components/guards/route.guard.tsx
+++ apps/app/src/components/guards/route.guard.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useRef } from 'react';
 import { useAuth } from 'react-oidc-context';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 
 export const RouteGuard = () => {
   const auth = useAuth();
+  const navigate = useNavigate();
+  const redirectDoneRef = useRef(false); // this is for double rendering in debug mode due to strict mode
+
+  useEffect(() => {
+    if (auth.isAuthenticated && !redirectDoneRef.current) {
+      const redirectPath = sessionStorage.getItem('redirectPath') || '/';
+      navigate(redirectPath, { replace: true });
+      sessionStorage.removeItem('redirectPath');
+      redirectDoneRef.current = true;
+    }
+  }, [auth.isAuthenticated, navigate]);
 
   if (!auth.isAuthenticated && !auth.isLoading) {
-    return <Navigate replace to="auth/login" />;
+    const currentPath = window.location.pathname;
+    sessionStorage.setItem('redirectPath', currentPath);
+    return <Navigate replace to="/auth/login" />;
   }
 
   return <Outlet />;
