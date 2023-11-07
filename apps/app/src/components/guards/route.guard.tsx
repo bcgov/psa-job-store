53b3b22b7656789ab7cc5c42cd2391dc6c2b2105
alex-struk <109102252+alex-struk@users.noreply.github.com>
2023-11-07 11:22:04 -0800

    Bug fix for "next" auth feature
    Bug fix where user would be redirected to home page after refreshing the page

--- apps/app/src/components/guards/route.guard.tsx
+++ apps/app/src/components/guards/route.guard.tsx
@@ -1,24 +1,26 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useAuth } from 'react-oidc-context';
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 
 export const RouteGuard = () => {
   const auth = useAuth();
   const navigate = useNavigate();
-  const redirectDoneRef = useRef(false); // this is for double rendering in debug mode due to strict mode
 
   useEffect(() => {
-    if (auth.isAuthenticated && !redirectDoneRef.current) {
-      const redirectPath = sessionStorage.getItem('redirectPath') || '/';
-      navigate(redirectPath, { replace: true });
-      sessionStorage.removeItem('redirectPath');
-      redirectDoneRef.current = true;
+    if (auth.isAuthenticated) {
+      const redirectPath = sessionStorage.getItem('redirectPath');
+      if (redirectPath) {
+        navigate(redirectPath, { replace: true });
+        sessionStorage.removeItem('redirectPath');
+      }
     }
   }, [auth.isAuthenticated, navigate]);
 
   if (!auth.isAuthenticated && !auth.isLoading) {
-    const currentPath = window.location.pathname;
-    sessionStorage.setItem('redirectPath', currentPath);
+    if (!sessionStorage.getItem('redirectPath')) {
+      const currentPath = window.location.pathname + window.location.search;
+      sessionStorage.setItem('redirectPath', currentPath);
+    }
     return <Navigate replace to="/auth/login" />;
   }
 
