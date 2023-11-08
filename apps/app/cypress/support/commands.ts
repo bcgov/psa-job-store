1b5f70260dd2e275a046a559aa1ec1d7eabcc8f4
alex-struk <109102252+alex-struk@users.noreply.github.com>
2023-11-08 08:08:47 -0800

    setup cypress with feature file parsing

---
+++ apps/app/cypress/support/commands.ts
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+Cypress.Commands.add('login', (username: string, password: string) => {
+  // Your login logic here.
+  cy.visit('/login'); // Replace with your app's login page URL.
+
+  // Replace the selectors with the appropriate ones for your login form.
+  cy.get('input[name=username]').type(username);
+  cy.get('input[name=password]').type(password);
+  cy.get('form').submit(); // Or the selector for your login button.
+
+  // Optionally add a check to ensure the login was successful.
+  cy.url().should('include', '/dashboard'); // Make sure the URL contains '/dashboard' or some path you expect after login.
+});
