6ba6ac322fd75497840d07ba7492b9aba97717a9
alex-struk <109102252+alex-struk@users.noreply.github.com>
2024-02-05 09:02:01 -0800

    status pages for HM views
    - Added location api
    - Refactored classification views into shared components
    -

--- apps/app/src/routes/wizard/position-request.page.tsx
+++ apps/app/src/routes/wizard/position-request.page.tsx
@@ -1,7 +1,13 @@
-import { Modal } from 'antd';
+import { ClockCircleFilled, ExclamationCircleFilled } from '@ant-design/icons';
+import { Alert, Button, Card, Col, Descriptions, Modal, Result, Row, Tabs, Typography } from 'antd';
+import Title from 'antd/es/typography/Title';
 import { useEffect, useState } from 'react';
 import { useBlocker, useParams } from 'react-router-dom';
 import { useGetPositionRequestQuery } from '../../redux/services/graphql-api/position-request.api';
+import { JobProfileWithDiff } from '../classification-tasks/components/job-profile-with-diff.component';
+import { ServiceRequestDetails } from '../classification-tasks/components/service-request-details.component';
+import ContentWrapper from '../home/components/content-wrapper.component';
+import OrgChartWrapped from '../org-chart/components/org-chart-wrapped.component';
 import { useWizardContext } from './components/wizard.provider';
 import { WizardConfirmDetailsPage } from './wizard-confirm-details.page';
 import { WizardEditPage } from './wizard-edit.page';
@@ -10,7 +16,18 @@ import { WizardResultPage } from './wizard-result.page';
 import { WizardReviewPage } from './wizard-review.page';
 import { WizardPage } from './wizard.page';
 
+const { Paragraph } = Typography;
+
 export const PositionRequestPage = () => {
+  let mode = 'editable';
+  mode = 'readonly';
+
+  mode = 'editable';
+
+  let readonlyMode = 'sentForVerification';
+  readonlyMode = 'reSubmittedForVerification';
+  readonlyMode = 'inQueue';
+
   const { setWizardData, setPositionRequestId, setPositionRequestProfileId, setPositionRequestDepartmentId } =
     useWizardContext();
   // console.log('wizardData: ', wizardData);
@@ -76,21 +93,220 @@ export const PositionRequestPage = () => {
     }
   };
 
+  // READONLY MODE
+
+  const snapshotCopy = JSON.parse(JSON.stringify(data?.positionRequest?.orgchart_json));
+
+  const tabItems = [
+    {
+      key: '1',
+      label: 'Job details',
+      children: <ServiceRequestDetails positionRequestData={data}></ServiceRequestDetails>,
+    },
+    {
+      key: '2',
+      label: 'Organization Chart',
+      children: (
+        <div style={{ overflow: 'hidden', position: 'relative', height: '800px' }}>
+          <OrgChartWrapped
+            selectedDepartment={data?.positionRequest?.department_id ?? null}
+            orgChartSnapshot={snapshotCopy}
+            highlightPositionId={'extraNode'}
+            extraNodeInfo={{
+              nodeId: 'extraNode',
+              title: data?.positionRequest?.title,
+              classification: { code: '', name: '' },
+              status: data?.positionRequest?.status,
+              targetNodeId: data?.positionRequest?.reports_to_position_id?.toString(),
+              submittedBy: data?.positionRequest?.user_name,
+            }}
+          />
+        </div>
+      ),
+    },
+    {
+      key: '3',
+      label: 'Job Profile',
+      children: <JobProfileWithDiff positionRequestData={data} />,
+    },
+    {
+      key: '4',
+      label: 'Actions',
+      children: (
+        <>
+          {readonlyMode === 'sentForVerification' && (
+            <>
+              <Result
+                icon={<ClockCircleFilled style={{ color: '#9254DE' }} />}
+                title="Sent for verification"
+                subTitle="The profile was submitted for review on: " // todo: add date
+              />
+
+              <Row justify="center" style={{ padding: '0 1rem' }}>
+                <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+                  <Alert
+                    message="We have received your verification request. We are working on the request and will respond. Depending on the information that needs to be reviewed, we will respond as quickly as possible."
+                    type="info"
+                    showIcon
+                    icon={<ExclamationCircleFilled />}
+                    style={{ marginBottom: '24px' }}
+                  />
+
+                  <Card title="Other Actions" bordered={false} style={{ marginBottom: '20rem' }}>
+                    {/* <Title level={5}>Save as Draft</Title>
+              <Paragraph>
+                Let's save your progress and come back later to make changes or get a position number.
+              </Paragraph>
+              <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+              <Divider /> */}
+
+                    {/* <Title level={5}>Allow others to edit</Title>
+              <Paragraph>
+                Share the URL with people who you would like to collaborate with (IDIR restricted).
+              </Paragraph>
+              <Button onClick={handleCopyURL}>Copy URL</Button>
+
+              <Divider /> */}
+
+                    <Title level={5}>View all Positions</Title>
+                    <Paragraph>View all positions that you have created.</Paragraph>
+                    <Button type="default" onClick={() => {}}>
+                      Go to Dashboard
+                    </Button>
+                  </Card>
+                </Col>
+              </Row>
+            </>
+          )}
+
+          {readonlyMode === 'reSubmittedForVerification' && (
+            <>
+              <Result
+                icon={<ClockCircleFilled style={{ color: '#9254DE' }} />}
+                title="Sent for verification"
+                subTitle="The profile was re-submitted for verification on:" // todo: add date
+              />
+
+              <Row justify="center" style={{ padding: '0 1rem' }}>
+                <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+                  <Alert
+                    message={
+                      <>
+                        <div>Service standards for profile review process submitted via JobStore:</div>
+                        <ul>
+                          <li>
+                            For profiles that need review by analyst or specialist: <b>21 business days</b>
+                          </li>
+                        </ul>
+                      </>
+                    }
+                    type="info"
+                    showIcon
+                    icon={<ExclamationCircleFilled />}
+                    style={{ marginBottom: '24px' }}
+                  />
+
+                  <Card title="Other Actions" bordered={false} style={{ marginBottom: '20rem' }}>
+                    {/* <Title level={5}>Save as Draft</Title>
+      <Paragraph>
+        Let's save your progress and come back later to make changes or get a position number.
+      </Paragraph>
+      <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+      <Divider /> */}
+
+                    {/* <Title level={5}>Allow others to edit</Title>
+      <Paragraph>
+        Share the URL with people who you would like to collaborate with (IDIR restricted).
+      </Paragraph>
+      <Button onClick={handleCopyURL}>Copy URL</Button>
+
+      <Divider /> */}
+
+                    <Title level={5}>View all Positions</Title>
+                    <Paragraph>View all positions that you have created.</Paragraph>
+                    <Button type="default" onClick={() => {}}>
+                      Go to Dashboard
+                    </Button>
+                  </Card>
+                </Col>
+              </Row>
+            </>
+          )}
+
+          {readonlyMode === 'inQueue' && (
+            <>
+              <Result
+                status="success"
+                title="Your position has been created."
+                subTitle="Find the information needed for recruitment in the table below."
+                extra={[
+                  <Button type="primary" key="console" onClick={() => {}}>
+                    Go to Dashboard
+                  </Button>,
+                ]}
+              />
+              <Row justify="center" style={{ padding: '0 1rem', marginBottom: '5rem' }}>
+                <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+                  <Card title="Information" bordered={false}>
+                    <Descriptions bordered layout="horizontal" column={1}>
+                      <Descriptions.Item label="Position number">
+                        123456 <Button type="link">Copy</Button>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Job Details">
+                        <Button type="link">View</Button> | <Button type="link">Download</Button>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Organization chart">
+                        <Button type="link">View</Button> | <Button type="link">Download</Button>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Job store job profile">
+                        <Button type="link">View</Button> | <Button type="link">Download</Button>
+                      </Descriptions.Item>
+                      <Descriptions.Item label="Modified job profile">
+                        <Button type="link">View</Button> | <Button type="link">Download</Button>
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </Card>
+                </Col>
+              </Row>
+            </>
+          )}
+        </>
+      ),
+    },
+  ];
+
   return (
     <>
-      {blocker.state === 'blocked' && (
-        <Modal
-          title="Are you sure you want to leave?"
-          open={true}
-          onOk={async () => {
-            blocker.proceed();
-          }}
-          onCancel={() => blocker.reset()}
-        >
-          <p>You can resume the process from "My Positions" page</p>
-        </Modal>
+      {mode === 'readonly' && (
+        <>
+          <ContentWrapper>
+            <Tabs
+              defaultActiveKey="1"
+              items={tabItems}
+              tabBarStyle={{ backgroundColor: '#fff', margin: '0 -1rem', padding: '0 1rem 0px 1rem' }}
+            />
+          </ContentWrapper>
+        </>
       )}
-      {currentStep !== null ? renderStepComponent() : <div>Loading position request information...</div>}
+      {mode === 'editable' && (
+        <>
+          {blocker.state === 'blocked' && (
+            <Modal
+              title="Are you sure you want to leave?"
+              open={true}
+              onOk={async () => {
+                blocker.proceed();
+              }}
+              onCancel={() => blocker.reset()}
+            >
+              <p>You can resume the process from "My Positions" page</p>
+            </Modal>
+          )}
+          {currentStep !== null ? renderStepComponent() : <div>Loading position request information...</div>}
+        </>
+      )}
     </>
   );
   // return <div>wizard position request page</div>;
--- apps/app/src/routes/wizard/wizard-result.page.tsx
+++ apps/app/src/routes/wizard/wizard-result.page.tsx
@@ -1,35 +1,347 @@
-import { Button, Result } from 'antd';
+import { CheckCircleOutlined, ExclamationCircleFilled, WarningFilled } from '@ant-design/icons';
+import { Alert, Button, Card, Col, Form, Modal, Result, Row } from 'antd';
+import Paragraph from 'antd/es/typography/Paragraph';
+import Title from 'antd/es/typography/Title';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { PageHeader } from '../../components/app/page-header.component';
+import { useUpdatePositionRequestMutation } from '../../redux/services/graphql-api/position-request.api';
+import ContentWrapper from '../home/components/content-wrapper.component';
 import { WizardSteps } from '../wizard/components/wizard-steps.component';
-import { WizardPageWrapper } from './components/wizard-page-wrapper.component';
+import { useWizardContext } from './components/wizard.provider';
 
 export const WizardResultPage = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/');
+  let mode = 'readyToCreatePositionNumber';
+  mode = 'verificationRequired_edits'; // verification is needed because user made edits to significant sections
+  mode = 'verificationRequired_retry'; // verification is needed because classifications team requested changes
+  mode = 'classificationReviewRequired'; // classification review is needed
+
+  mode = 'readyToCreatePositionNumber';
+
+  const { positionRequestId } = useWizardContext();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [updatePositionRequest] = useUpdatePositionRequestMutation();
+
+  const showModal = async () => {
+    setIsModalVisible(true);
   };
 
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleOk = async () => {
+    // User pressed next on the review screen
+    // A modal appeared with terms
+    // User confirmed the terms in the modal by pressing OK
+
+    // Hide the modal
+    setIsModalVisible(false);
+
+    try {
+      if (positionRequestId) {
+        await updatePositionRequest({
+          id: positionRequestId,
+          status: 'COMPLETE',
+          step: 6,
+        }).unwrap();
+      } else {
+        throw Error('Position request not found');
+      }
+    } catch (error) {
+      // Handle the error, possibly showing another modal
+      Modal.error({
+        title: 'Error Creating Position',
+        content: 'An unknown error occurred', //error.data?.message ||
+      });
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const handleSendForReview = async () => {};
+
   return (
-    <WizardPageWrapper title="Result" subTitle="Find out the result of your request" xxl={14} xl={18}>
-      <WizardSteps current={5} xl={24}></WizardSteps>
-      <Result
-        status="success"
-        title="Your position has been created."
-        subTitle={
-          <div>
-            <div>Position: Program Assistant (L9)</div>
-            <br />
-            <div>Position #: 12345678</div>
-            <br />
-            <div>We have submitted a hiring request for this position. A recruiter will reach out to you shortly.</div>
+    <>
+      <PageHeader title="Result" subTitle="Find out the result of your request" />
+
+      <Row justify="center" style={{ padding: '0 1rem' }}>
+        <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+          <div style={{ background: 'white' }}>
+            <WizardSteps current={5} xl={24}></WizardSteps>
           </div>
-        }
-        extra={[
-          <Button type="primary" key="console" onClick={handleClick}>
-            Go to Dashboard
+        </Col>
+      </Row>
+
+      {mode === 'readyToCreatePositionNumber' && (
+        <ContentWrapper>
+          <Result icon={<CheckCircleOutlined />} title="The job profile is ready!" />
+
+          <Row justify="center" style={{ padding: '0 1rem' }}>
+            <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+              <Alert
+                description="You have completed your profile. At this point, it is saved as draft. You may ask others to review by sending them a link, or you may proceed to generating a position number. If there have been changes to significant accountabilities, we will seamlessly create a position review for classification and exclusion services."
+                type="info"
+                showIcon
+              />
+
+              <Card title="Get position number" bordered={false} style={{ marginTop: '1rem' }}>
+                <Form layout="vertical">
+                  <Form.Item name="jobTitle" labelCol={{ className: 'card-label' }} colon={false}>
+                    <div style={{ margin: 0 }}>
+                      The job profile meets all the criteria, you can generate a position number (this action is
+                      irreversable).
+                    </div>
+                  </Form.Item>
+                  <Button type="primary" onClick={showModal}>
+                    Generate position number
+                  </Button>
+                </Form>
+              </Card>
+
+              <Card title="Other actions" bordered={false} style={{ marginTop: '1rem' }}>
+                {/* <Typography.Title level={5}>Send for Review</Typography.Title>
+              <Typography.Paragraph>
+                This Job Profile is not ready to be published yet. There seems to me many changes to the profile, so you
+                would first need to send this for a classification review.
+              </Typography.Paragraph>
+              <Button onClick={handleClick}>Send for Review</Button>
+              <Divider /> */}
+
+                {/* <Title level={5}>Save as Draft</Title>
+              <Paragraph>
+                Let’s save your progress and come back later to make changes or get a position number.
+              </Paragraph>
+              <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+              <Divider /> */}
+
+                {/* <Title level={5}>Allow others to edit</Title>
+              <Paragraph>Share the URL with people who you would like to collaborate with (IDIR restricted).</Paragraph>
+              <Button onClick={handleCopyURL}>Copy URL</Button>
+
+              <Divider /> */}
+
+                <Title level={5}>View all Positions</Title>
+                <Paragraph>View all positions that you have created.</Paragraph>
+                <Button type="default" onClick={() => navigate('/')}>
+                  Go to Dashboard
+                </Button>
+              </Card>
+            </Col>
+          </Row>
+        </ContentWrapper>
+      )}
+
+      {mode === 'verificationRequired_edits' && (
+        <ContentWrapper>
+          <Result icon={<WarningFilled />} title="Verification required" status="warning" />
+
+          <Row justify="center" style={{ padding: '0 1rem' }}>
+            <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+              <Alert
+                message=""
+                description="Some of your amendments to the generic profile require verification. If you would like to revisit some of your amendments, please click these links: Accountabilities, Job requirements."
+                type="warning"
+                showIcon
+                icon={<ExclamationCircleFilled />}
+                style={{ marginBottom: '24px' }}
+              />
+
+              <Card title="Send for verification" bordered={false} style={{ marginBottom: '1rem' }}>
+                <Paragraph>
+                  Forward the profile to be verified by the classifications team. We will respond shortly after
+                  verification, once submitted. There are no other steps required, just look for our followup response.
+                </Paragraph>
+                <Button type="primary" onClick={handleSendForReview}>
+                  Submit for verification
+                </Button>
+              </Card>
+
+              <Card title="Other Actions" bordered={false}>
+                {/* <Title level={5}>Save as Draft</Title>
+                <Paragraph>
+                  Let's save your progress and come back later to make changes or get a position number.
+                </Paragraph>
+                <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+                <Divider /> */}
+
+                {/* <Title level={5}>Allow others to edit</Title>
+                <Paragraph>
+                  Share the URL with people who you would like to collaborate with (IDIR restricted).
+                </Paragraph>
+                <Button onClick={handleCopyURL}>Copy URL</Button>
+
+                <Divider /> */}
+
+                <Title level={5}>View all Positions</Title>
+                <Paragraph>View all positions that you have created.</Paragraph>
+                <Button type="default" onClick={() => navigate('/')}>
+                  Go to Dashboard
+                </Button>
+              </Card>
+            </Col>
+          </Row>
+        </ContentWrapper>
+      )}
+
+      {mode === 'verificationRequired_retry' && (
+        <ContentWrapper>
+          <Result icon={<WarningFilled />} title="Verification required" status="warning" />
+
+          <Row justify="center" style={{ padding: '0 1rem' }}>
+            <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+              <Alert
+                message=""
+                description="To ensure that the verification goes successfully, make sure that you have made all the changes as suggested by the classification team."
+                type="warning"
+                showIcon
+                icon={<ExclamationCircleFilled />}
+                style={{ marginBottom: '24px' }}
+              />
+
+              <Card title="Send for verification" bordered={false} style={{ marginBottom: '1rem' }}>
+                <Paragraph>
+                  There are changes to the profile which needs to be verified by the classifications team.
+                </Paragraph>
+                <Button type="primary" onClick={handleSendForReview}>
+                  Submit for verification
+                </Button>
+              </Card>
+
+              <Card title="Other Actions" bordered={false}>
+                {/* <Title level={5}>Save as Draft</Title>
+                <Paragraph>
+                  Let's save your progress and come back later to make changes or get a position number.
+                </Paragraph>
+                <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+                <Divider /> */}
+
+                {/* <Title level={5}>Allow others to edit</Title>
+                <Paragraph>
+                  Share the URL with people who you would like to collaborate with (IDIR restricted).
+                </Paragraph>
+                <Button onClick={handleCopyURL}>Copy URL</Button>
+
+                <Divider /> */}
+
+                <Title level={5}>View all Positions</Title>
+                <Paragraph>View all positions that you have created.</Paragraph>
+                <Button type="default" onClick={() => navigate('/')}>
+                  Go to Dashboard
+                </Button>
+              </Card>
+            </Col>
+          </Row>
+        </ContentWrapper>
+      )}
+
+      {mode === 'classificationReviewRequired' && (
+        <ContentWrapper>
+          <Result
+            icon={<WarningFilled />}
+            title="Classification review required"
+            status="warning"
+            subTitle="A member of the classification team will reach out to you via email shortly."
+          />
+
+          <Row justify="center" style={{ padding: '0 1rem' }}>
+            <Col xs={24} md={24} lg={24} xl={14} xxl={18}>
+              <Alert
+                message="The initial verification has indicated your request requires a classification review. To send for a classification review click below. A Classification Specialist will then reach out shortly via email."
+                type="warning"
+                showIcon
+                icon={<ExclamationCircleFilled />}
+                style={{ marginBottom: '24px' }}
+              />
+
+              <Card title="Send for verification" bordered={false} style={{ marginBottom: '1rem' }}>
+                <Paragraph>
+                  There are changes to the profile which needs to be reviewed by a specialist in the classifications
+                  team.
+                </Paragraph>
+                <Button type="primary" onClick={handleSendForReview}>
+                  Send for classification review
+                </Button>
+              </Card>
+
+              <Card title="Other Actions" bordered={false}>
+                {/* <Title level={5}>Save as Draft</Title>
+                <Paragraph>
+                  Let's save your progress and come back later to make changes or get a position number.
+                </Paragraph>
+                <Button onClick={handleSaveAsDraft}>Save as Draft</Button>
+
+                <Divider /> */}
+
+                {/* <Title level={5}>Allow others to edit</Title>
+                <Paragraph>
+                  Share the URL with people who you would like to collaborate with (IDIR restricted).
+                </Paragraph>
+                <Button onClick={handleCopyURL}>Copy URL</Button>
+
+                <Divider /> */}
+
+                <Title level={5}>View all Positions</Title>
+                <Paragraph>View all positions that you have created.</Paragraph>
+                <Button type="default" onClick={() => navigate('/')}>
+                  Go to Dashboard
+                </Button>
+              </Card>
+            </Col>
+          </Row>
+        </ContentWrapper>
+      )}
+
+      <Modal
+        title="Affirmation"
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Generate position number
           </Button>,
         ]}
-      />
-    </WizardPageWrapper>
+      >
+        <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+          <div>
+            <b>By clicking “Generate position number” I affirm that:</b>
+            <ul>
+              <li>
+                I confirm this Statement of Job Responsibilities accurately reflects the actual work to be performed of
+                the position(s) as outlined in{' '}
+                <a
+                  target="_blank"
+                  href="https://www2.gov.bc.ca/assets/gov/careers/managers-supervisors/managing-employee-labour-relations/hr-policy-pdf-documents/06_job_evaluation_policy.pdf"
+                >
+                  Human Resources Policy 06 – Job Evaluation
+                </a>
+                , and
+              </li>
+              <li>
+                I confirm the accountabilities are not similar to the supervisor, peer, or management positions within
+                the work unit, and
+              </li>
+              <li>
+                As the excluded manager or delegate, I confirm the job role, accountabilities, and scope of
+                responsibility are true and accurate, and in establishing this position (s), I confirm the content I
+                assume all risks related to this decision.{' '}
+              </li>
+              <li>I will respond to audits in a timely manner.</li>
+              <li>
+                I will abide by the Public Service Act and all Human Resources policies for hiring decisions related to
+                this position.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
