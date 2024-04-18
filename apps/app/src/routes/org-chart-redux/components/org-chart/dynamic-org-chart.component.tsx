8b46e2aafc96d997f687b79948d091f3e719df61
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-04-18 08:39:08 -0700

    add new org chart to hoem page

--- apps/app/src/routes/org-chart-redux/components/org-chart/dynamic-org-chart.component.tsx
+++ apps/app/src/routes/org-chart-redux/components/org-chart/dynamic-org-chart.component.tsx
@@ -1,4 +1,4 @@
-import { Col, Row, Space, Tag } from 'antd';
+import { Col, Row, Space } from 'antd';
 import Fuse from 'fuse.js';
 import { useEffect, useMemo, useState } from 'react';
 import ReactFlow, {
@@ -193,13 +193,6 @@ export const DynamicOrgChart = ({
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '100%' }}>
       <Row gutter={[8, 8]} justify="space-between" style={{ margin: '0.5rem 1rem' }}>
-        <Col span={24}>
-          <Tag>Filters</Tag>
-          <Tag color={isDirty ? 'red' : 'green'}>Is Dirty?: {isDirty === true ? 'true' : 'false'}</Tag>
-          <Tag color="green">Department ID: {departmentId ?? ''}</Tag>
-          <Tag color="green">Search Term: {searchTerm ?? ''}</Tag>
-          {/* <Tag color="green">Search Results: {searchResultNodes?.length}</Tag> */}
-        </Col>
         <Col xs={24} md={{ offset: 12, span: 12 }}>
           <Space direction="vertical" style={{ width: '100%' }}>
             <DepartmentFilter
