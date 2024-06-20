cb5a8883f0652177b93018cd6cbdb27d2c45f5c8
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-06-20 13:18:53 -0700

    create user module, independent user sync process, and fix the guid/uuid issue.

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -4,13 +4,15 @@ import { CrmService } from '../external/crm.service';
 import { ExternalModule } from '../external/external.module';
 import { PeoplesoftService } from '../external/peoplesoft.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { ProfileResolver } from './profile.resolver';
 import { KeycloakStrategy } from './strategies/keycloak.strategy';
 
 @Module({
-  imports: [HttpModule, PrismaModule, ExternalModule],
-  providers: [AuthService, CrmService, KeycloakStrategy, PeoplesoftService, ProfileResolver],
+  imports: [HttpModule, PrismaModule, ExternalModule, UserModule],
+  providers: [AuthService, CrmService, KeycloakStrategy, PeoplesoftService, ProfileResolver, UserService],
   exports: [AuthService],
 })
 export class AuthModule {}
--- apps/api/src/modules/scheduled-task/scheduled-task.module.ts
+++ apps/api/src/modules/scheduled-task/scheduled-task.module.ts
@@ -2,13 +2,17 @@ import { HttpModule } from '@nestjs/axios';
 import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common';
 import { CrmService } from '../external/crm.service';
 import { ExternalModule } from '../external/external.module';
+import { KeycloakService } from '../external/keycloak.service';
+import { PeoplesoftV2Service } from '../external/peoplesoft-v2.service';
 import { PeoplesoftService } from '../external/peoplesoft.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
 import { ScheduledTaskService } from './scheduled-task.service';
 
 @Module({
-  imports: [ExternalModule, HttpModule, PrismaModule],
-  providers: [CrmService, PeoplesoftService, ScheduledTaskService],
+  imports: [ExternalModule, HttpModule, PrismaModule, UserModule],
+  providers: [CrmService, KeycloakService, PeoplesoftService, PeoplesoftV2Service, ScheduledTaskService, UserService],
 })
 export class ScheduledTaskModule implements OnApplicationBootstrap {
   private readonly logger: Logger = new Logger(ScheduledTaskModule.name);
@@ -18,6 +22,7 @@ export class ScheduledTaskModule implements OnApplicationBootstrap {
   async onApplicationBootstrap() {
     await this.scheduledTaskService.syncPositionStatuses();
     await this.scheduledTaskService.syncPeoplesoftData();
+    await this.scheduledTaskService.syncUsers();
   }
 }
 //
