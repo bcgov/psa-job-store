e1f59704520f81091139721c2e24f8a001968f26
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-06-28 15:30:14 -0700

    add endpoint to set user org chart access

--- apps/app/src/redux/services/graphql-api/settings/settings.api.ts
+++ apps/app/src/redux/services/graphql-api/settings/settings.api.ts
@@ -3,6 +3,8 @@ import { graphqlApi } from '..';
 import { GetOrganizationsResponse } from './dtos/get-organizations-response.dto';
 import { GetUserResponse } from './dtos/get-user-response.dto';
 import { GetUsersResponse } from './dtos/get-users-response.dto';
+import { SetUserOrgChartAccessInput } from './dtos/set-user-org-chart-access-input.dto';
+import { SetUserOrgChartAccessResponse } from './dtos/set-user-org-chart-access-resposne.dto';
 
 export const settingsApi = graphqlApi.injectEndpoints({
   endpoints: (build) => ({
@@ -61,14 +63,37 @@ export const settingsApi = graphqlApi.injectEndpoints({
         },
       }),
     }),
+    setUserOrgChartAccess: build.mutation<SetUserOrgChartAccessResponse, SetUserOrgChartAccessInput>({
+      query: (input: SetUserOrgChartAccessInput) => ({
+        document: gql`
+          mutation SetUserOrgChartAccess($data: SetUserOrgChartAccessInput!) {
+            setUserOrgChartAccess(data: $data) {
+              id
+              name
+              email
+              username
+              roles
+              metadata
+              created_at
+              updated_at
+              deleted_at
+            }
+          }
+        `,
+        variables: {
+          data: input,
+        },
+      }),
+    }),
   }),
 });
 
 export const {
   useLazyGetOrganizationsQuery,
-  useGetOrganizationsQuery,
   useLazyGetUserQuery,
-  useGetUserQuery,
   useLazyGetUsersQuery,
+  useGetOrganizationsQuery,
+  useGetUserQuery,
   useGetUsersQuery,
+  useSetUserOrgChartAccessMutation,
 } = settingsApi;
