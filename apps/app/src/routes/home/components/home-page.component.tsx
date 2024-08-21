dafc0f16d62cd491eaf8c97b3e4883178be6bab1
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-08-21 15:21:00 -0700

    Hook up new pages for default, super-admin, use existing pages for hiring-manager, classification, total-compensation.

--- apps/app/src/routes/home/components/home-page.component.tsx
+++ apps/app/src/routes/home/components/home-page.component.tsx
@@ -2,22 +2,24 @@
 import { useMemo } from 'react';
 import { useAuth } from 'react-oidc-context';
 import PositionProfile from '../../../components/app/common/components/positionProfile';
+import { PositionProvider } from '../../../components/app/common/contexts/position.context';
 import { PageHeader } from '../../../components/app/page-header.component';
 import ContentWrapper from '../../../components/content-wrapper.component';
 import { useGetProfileQuery } from '../../../redux/services/graphql-api/profile.api';
 import { getUserRoles } from '../../../utils/get-user-roles.util';
+import { ClassificationTasksPage } from '../../classification-tasks/classification-tasks.page';
+import { HomePage as LegacyHomePage } from '../../home-old/home.page';
+import { TotalCompDraftProfilesPage } from '../../total-comp-draft-profiles/total-comp-draft-profies.page';
 import { getInitialsAvatarProps } from '../utils/get-initials-avatar-props.util';
 import { DefaultHomePage } from './default-home-page/default-home-page.component';
 import { SuperAdminHomePage } from './super-admin-home-page.component';
 
-export const HomePage = () => {
+const NewHomePage = () => {
   const auth = useAuth();
   const roles = getUserRoles(auth.user);
 
   const { family_name, given_name } = (auth.user?.profile ?? {}) as Record<string, any>;
   const { data: profileData } = useGetProfileQuery();
-
-  // auth.user.profile.given_name/family_name
 
   const renderHomePageForRole = useMemo(() => {
     if (roles.includes('super-admin')) {
@@ -33,7 +35,7 @@ export const HomePage = () => {
     }
   }, [roles]);
 
-  return (
+  return auth.isAuthenticated ? (
     <>
       <PageHeader
         avatar={getInitialsAvatarProps(given_name, family_name)}
@@ -49,5 +51,38 @@ export const HomePage = () => {
       />
       <ContentWrapper>{renderHomePageForRole}</ContentWrapper>
     </>
+  ) : (
+    <></>
   );
+};
+
+export const HomePage = () => {
+  const auth = useAuth();
+  const roles = getUserRoles(auth.user);
+
+  // auth.user.profile.given_name/family_name
+
+  if (roles.includes('super-admin')) {
+    return <NewHomePage />;
+  } else if (roles.includes('total-compensation')) {
+    return (
+      <PositionProvider>
+        <TotalCompDraftProfilesPage />
+      </PositionProvider>
+    );
+  } else if (roles.includes('classification')) {
+    return (
+      <PositionProvider>
+        <ClassificationTasksPage />
+      </PositionProvider>
+    );
+  } else if (roles.includes('hiring-manager')) {
+    return (
+      <PositionProvider>
+        <LegacyHomePage />
+      </PositionProvider>
+    );
+  } else {
+    return <NewHomePage />;
+  }
 };
