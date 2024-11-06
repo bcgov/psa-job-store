904307222ccf0a3ae35a6dbb84ee22b6e7f091ff
alex-struk <109102252+alex-struk@users.noreply.github.com>
2024-11-06 08:53:23 -0800

    Update job-profile.component.tsx

--- apps/app/src/routes/job-profiles/components/job-profile.component.tsx
+++ apps/app/src/routes/job-profiles/components/job-profile.component.tsx
@@ -528,7 +528,7 @@ export const JobProfile: React.FC<JobProfileProps> = ({
     hideDisabled?: boolean,
   ): JSX.Element[] => {
     const comparisonResult: JSX.Element[] = [];
-    // console.log('original/modified: ', original, modified);
+    console.log('original/modified: ', original, modified);
     if (!modified) return comparisonResult;
 
     // Add this check to handle null original
