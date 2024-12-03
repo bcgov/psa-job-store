ca4da9d554a6ad725341743732cdfaa24ab2f3ee
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-12-03 13:27:15 -0800

    update vite env vars

--- apps/jobstore-cypress/cypress/support/commands.ts
+++ apps/jobstore-cypress/cypress/support/commands.ts
@@ -17,33 +17,34 @@ Cypress.Commands.add('login', () => {
         cy.log('Logging in 3...');
         Cypress.env('AUTH_TOKEN', body.access_token);
         // Store token in the same format as your current setup
-        window.localStorage.setItem(
-          `oidc.user:${Cypress.env('VITE_KEYCLOAK_REALM_URL')}:${Cypress.env('VITE_KEYCLOAK_CLIENT_ID')}`,
-          JSON.stringify({
-            access_token: body.access_token,
-            profile: {
-              exp: Math.floor(Date.now() / 1000) + 3600,
-              iat: Math.floor(Date.now() / 1000),
-              iss: 'xxx',
-              aud: 'xxx',
-              sub: 'xxx',
-              typ: 'ID',
-              session_state: 'xxx',
-              sid: 'xxx',
-              idir_user_guid: 'xxx',
-              client_roles: ['hiring-manager'],
-              identity_provider: 'idir',
-              idir_username: 'TEST',
-              email_verified: false,
-              name: 'Test, User CITZ:EX',
-              preferred_username: 'xxx@idir',
-              display_name: 'Test, User CITZ:EX',
-              given_name: 'Test',
-              family_name: 'User',
-              email: 'test.user@gov.bc.ca',
-            },
-          }),
-        );
+        // TODO: AL-1020 fix this
+        // window.localStorage.setItem(
+        //   `oidc.user:${Cypress.env('VITE_KEYCLOAK_REALM_URL')}:${Cypress.env('VITE_KEYCLOAK_CLIENT_ID')}`,
+        //   JSON.stringify({
+        //     access_token: body.access_token,
+        //     profile: {
+        //       exp: Math.floor(Date.now() / 1000) + 3600,
+        //       iat: Math.floor(Date.now() / 1000),
+        //       iss: 'xxx',
+        //       aud: 'xxx',
+        //       sub: 'xxx',
+        //       typ: 'ID',
+        //       session_state: 'xxx',
+        //       sid: 'xxx',
+        //       idir_user_guid: 'xxx',
+        //       client_roles: ['hiring-manager'],
+        //       identity_provider: 'idir',
+        //       idir_username: 'TEST',
+        //       email_verified: false,
+        //       name: 'Test, User CITZ:EX',
+        //       preferred_username: 'xxx@idir',
+        //       display_name: 'Test, User CITZ:EX',
+        //       given_name: 'Test',
+        //       family_name: 'User',
+        //       email: 'test.user@gov.bc.ca',
+        //     },
+        //   }),
+        // );
       });
     },
     {
