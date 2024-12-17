038fb3c523977f1d63eb29fa0fa5a8621b82de98
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-12-17 10:59:02 -0800

    enhance logout endpoint

--- apps/api/src/modules/auth/controllers/auth.controller.ts
+++ apps/api/src/modules/auth/controllers/auth.controller.ts
@@ -3,6 +3,7 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
+import { Issuer } from 'openid-client';
 import { AppConfigDto } from '../../../dtos/app-config.dto';
 import { PublicRoute } from '../decorators/public-route.decorator';
 import { BCeIDLoginGuard } from '../guards/bceid-login.guard';
@@ -60,24 +61,25 @@ export class AuthController {
   async logout(@Req() req: Request, @Res() res: Response) {
     const { id_token } = (req.user as any) ?? { id_token: undefined };
 
-    await req.logout({ keepSessionInfo: false }, (error: any) => {});
-    // await req.session.destroy(async (error: any) => {
-    //   const TrustIssuer = await Issuer.discover(
-    //     `${this.configService.get('KEYCLOAK_REALM_URL')}/.well-known/openid-configuration`,
-    //   );
-    //   const end_session_endpoint = TrustIssuer.metadata.end_session_endpoint;
-    //   if (end_session_endpoint) {
-    //     res.redirect(
-    //       end_session_endpoint +
-    //         '?post_logout_redirect_uri=' +
-    //         this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL') +
-    //         (id_token ? '&id_token_hint=' + id_token : ''),
-    //     );
-    //   } else {
-    //     res.redirect(this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL'));
-    //   }
-    // });
+    await req.session.destroy(async (error: any) => {
+      await req.logout({ keepSessionInfo: false }, (error: any) => {});
 
-    res.redirect(this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL'));
+      const TrustIssuer = await Issuer.discover(
+        `${this.configService.get('KEYCLOAK_REALM_URL')}/.well-known/openid-configuration`,
+      );
+      const end_session_endpoint = TrustIssuer.metadata.end_session_endpoint;
+      if (end_session_endpoint) {
+        res.redirect(
+          end_session_endpoint +
+            '?post_logout_redirect_uri=' +
+            this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL') +
+            (id_token ? '&id_token_hint=' + id_token : ''),
+        );
+      } else {
+        res.redirect(this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL'));
+      }
+    });
+
+    // res.redirect(this.configService.get('KEYCLOAK_LOGOUT_REDIRECT_URL'));
   }
 }
