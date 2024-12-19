7be46791c404749dd942dd791bce3e9686da4eac
alex-struk <109102252+alex-struk@users.noreply.github.com>
2024-12-19 12:50:06 -0800

    unit testing
    prisma client mock

---
+++ apps/api/test/prisma.mock.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+import { mockDeep } from 'jest-mock-extended';
+
+export const prismaMock = mockDeep<PrismaClient>();
+
+jest.mock('@prisma/client', () => ({
+  PrismaClient: jest.fn(),
+  Prisma: {
+    defineExtension: jest.fn().mockImplementation((callback) => callback(mockDeep())),
+  },
+}));
